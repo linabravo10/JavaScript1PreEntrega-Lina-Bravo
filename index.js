7fd76bf8b2a9361354b4bcded5b5993ff6d99e60
linabravo10 <linabravo1012@gmail.com>
2023-06-06 01:33:01 -0500

    Se modifica el total de productos

--- index.js
+++ index.js
@@ -12,15 +12,43 @@ else {
 console.log(nombrePersona)
 
 const OPCION_SALIR = 4;
+const IVA = .19;
+let totalDeProducto = 100
 
-let opcion = parseInt(prompt(nombrePersona + " " + "elige la acción que deseas realizar, \n 1.Ver nuestros productos \n 2.Ver nuestros servicios \n 3.Comunicarte con servicio al cliente \n 4.Salir"));
+const verProductos = () =>{
+    let opcion;
+    opcion = parseInt(prompt(nombrePersona + " " + "elige la acción que deseas realizar, \n 1.Ver collares \n 2.Ver placas \n 3.Regresar a pagina anterior. \n 4.Salir"));
+    while(opcion !== OPCION_SALIR){
+        switch(opcion){
+            case 1: verCollares();
+                break;
+            case 2: verPlacas();
+                    break;
+            case 3: regresar();
+                    break;
+            case 4:
+                    break;
+            default:
+                alert("Acción no reconocida, intentalo de nuevo")
+        }
+    }
+}
+
+const verTotal = (total) =>{
+    const resultado = IVA + totalDeProducto;
+    alert("El total de los productos más IVA es " + resultado)
+
+}
+   
+
+let opcion = parseInt(prompt(nombrePersona + " " + "elige la acción que deseas realizar, \n 1.Ver nuestros productos \n 2.Ver el valor total del carrito de compras. \n 3.Comunicarte con servicio al cliente \n 4.Salir"));
 
 while(opcion != OPCION_SALIR){
 
     switch(opcion){
         case 1: verProductos();
                 break;
-        case 2: verServicios();
+        case 2: verTotal(totalDeProducto);
                 break;
         case 3: servicioCliente();
                 break;
@@ -29,7 +57,7 @@ while(opcion != OPCION_SALIR){
         default:
             alert("Acción no reconocida, intentalo de nuevo")
     }
-    opcion = parseInt(prompt(nombrePersona + "Elige la accion que deseas realizar, \n 1.Ver productos \n 2.Ver nuestros servicios \n 3.Servicio al cliente \n 4.Salir"));
+    opcion = parseInt(prompt(nombrePersona + "Elige la accion que deseas realizar, \n 1.Ver productos \n 2.Ver el valor total del carrito de compras. \n 3.Servicio al cliente \n 4.Salir"));
 }
 
 alert("Gracias por su visita!")
