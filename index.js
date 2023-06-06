7829e8b9ec0b770f774d3118b93439f05f7a7c03
linabravo10 <linabravo1012@gmail.com>
2023-06-06 00:15:52 -0500

    Se guarda inicio de proyecto

---
+++ index.js
@@ -0,0 +1,9 @@
+const OPCION_SALIR = 4;
+
+let opcion = prompt("Elige la accion que deseas realizar, \n 1.Ver nuestros productos \n 2.Ver nuestros servicios \n 3.Comunicarte con servicio al cliente \n 4.Salir");
+
+while(opcion != OPCION_SALIR){
+    prompt("Elige la accion que deseas realizar, \n 1.Ver productos \n 2.Ver nuestros servicios \n 3.Servicio al cliente \n 4.Salir");
+}
+
+alert("Gracias por su visita!")
