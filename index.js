47469426e055e992ad33f92ebcd857ae528010d6
linabravo10 <linabravo1012@gmail.com>
2023-06-22 00:32:52 -0500

    Se soluciona minuscula a mayuscula que no dejaba que el menu se desarrollara bien

--- index.js
+++ index.js
@@ -16,33 +16,53 @@ const IVA = .19;
 let totalDeProducto = 100
 const ArrayDeProductos = [];
 class Producto {
-    constructor(nombre, precio, descipcion, categoria){
+    constructor(nombre, precio, descripcion, categoria){
         this.precio = precio;
         this.nombre = nombre;
-        this.descripcion = descipcion;
+        this.descripcion = descripcion;
         this.categoria = categoria
     }
 }
 
 
-const collar1 = new Producto("Collar cuero", 40000, "Collar super comodo y con mucho estilo para tu peludito");
+const collar1 = new Producto("Collar cuero", 40000, "Collar súper cómodo y con mucho estilo para tu peludito", "Collares");
 ArrayDeProductos.push(collar1);
 
-const collar2 = new Producto("Collar rastreador", 60.000, "Collar con rastreador donde puedes ubicar a tu mascota.");
+const collar2 = new Producto("Collar rastreador", 60000, "Collar con rastreador donde puedes ubicar a tu mascota.", "Collares");
 ArrayDeProductos.push(collar2);
 
-const placa1 = new Producto("Placa marcada en laser", 15.000, "Placa con el nombre de tu mascota, tu nombre y numero telefonico");
+const placa1 = new Producto("Placa marcada en laser", 15000, "Placa con el nombre de tu mascota, tu nombre y número telefónico", "Placas");
 ArrayDeProductos.push(placa1);
 
-const placa2 = new Producto("Placa con codigo QR", 15.000, "Placa con codigo QR con todos tus datos personales");
+const placa2 = new Producto("Placa con codigo QR", 15000, "Placa con código QR con todos tus datos personales", "Placas");
 ArrayDeProductos.push(placa2);
 
-console.log(ArrayDeProductos)
+//console.log(ArrayDeProductos)
+
+
+const verCollares = () =>{
+    const filtrado = ArrayDeProductos.filter((el)=> el.categoria === 'Collares'); 
+    let mensajeAMostrar = '';
+    filtrado.forEach((el) =>{
+        mensajeAMostrar += "\nProducto: " + el.nombre + " " + "\nPrecio: " + el.precio
+    });
+    alert(mensajeAMostrar);
+}
+
+const verPlacas = () =>{
+    const filtrado = ArrayDeProductos.filter((el)=> el.categoria === 'Placas'); 
+    let mensajeAMostrar = '';
+    filtrado.forEach((el) =>{
+        mensajeAMostrar += "\nProducto: " + el.nombre + " " + "\nPrecio: " + el.precio
+    });
+    alert(mensajeAMostrar);
+}
+
 
 
 const verProductos = () =>{
     let opcion;
-    opcion = parseInt(prompt(nombrePersona + " " + "elige la acción que deseas realizar, \n 1.Ver collares \n 2.Ver placas \n 3.Regresar a pagina anterior. \n 4.Salir"));
+    opcion = parseInt(prompt(nombrePersona + " " + "Elige la acción que deseas realizar, \n 1.Ver collares \n 2.Ver placas \n 3.Regresar a pagina anterior. \n 4.Salir"));
     while(opcion !== OPCION_SALIR){
         switch(opcion){
             case 1: verCollares();
@@ -56,6 +76,7 @@ const verProductos = () =>{
             default:
                 alert("Acción no reconocida, intentalo de nuevo")
         }
+        opcion = parseInt(prompt("Elige la opción deseada:, \n 1-Collares \n 2-Placas"))
     }
 }
 
@@ -64,7 +85,7 @@ const verTotal = (total) =>{
     alert("El total de los productos más IVA es " + resultado)
 
 }
-   
+
 
 let opcion = parseInt(prompt(nombrePersona + " " + "elige la acción que deseas realizar, \n 1.Ver nuestros productos \n 2.Ver el valor total del carrito de compras. \n 3.Comunicarte con servicio al cliente \n 4.Salir"));
 
