bece4fe1ade4dda9bb3ffe54fb188a2fb3cfc3cd
linabravo10 <linabravo1012@gmail.com>
2023-06-21 23:26:49 -0500

    Se realizan modificaciones para segunda pre entrega

--- index.js
+++ index.js
@@ -14,6 +14,31 @@ console.log(nombrePersona)
 const OPCION_SALIR = 4;
 const IVA = .19;
 let totalDeProducto = 100
+const ArrayDeProductos = [];
+class Producto {
+    constructor(nombre, precio, descipcion, categoria){
+        this.precio = precio;
+        this.nombre = nombre;
+        this.descripcion = descipcion;
+        this.categoria = categoria
+    }
+}
+
+
+const collar1 = new Producto("Collar cuero", 40000, "Collar super comodo y con mucho estilo para tu peludito");
+ArrayDeProductos.push(collar1);
+
+const collar2 = new Producto("Collar rastreador", 60.000, "Collar con rastreador donde puedes ubicar a tu mascota.");
+ArrayDeProductos.push(collar2);
+
+const placa1 = new Producto("Placa marcada en laser", 15.000, "Placa con el nombre de tu mascota, tu nombre y numero telefonico");
+ArrayDeProductos.push(placa1);
+
+const placa2 = new Producto("Placa con codigo QR", 15.000, "Placa con codigo QR con todos tus datos personales");
+ArrayDeProductos.push(placa2);
+
+console.log(ArrayDeProductos)
+
 
 const verProductos = () =>{
     let opcion;
